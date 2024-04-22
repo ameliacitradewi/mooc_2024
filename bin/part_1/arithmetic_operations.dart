71b3933fc0c0a3e94e0cf3ab30f815235b6d597d
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-23 00:08:32 +0700

    commit

--- bin/part_1/arithmetic_operations.dart
+++ bin/part_1/arithmetic_operations.dart
@@ -2,19 +2,38 @@ import 'dart:io';
 
 void main() {
   ///Programming excercise: Times five
-  stdout.writeln("Please type in a number: ");
+  stdout.write("Please type in a number: ");
   int inputNumber = int.parse(stdin.readLineSync()!);
 
   print("$inputNumber times 5 is ${inputNumber * 5}");
-  // test
 
   ///Programming excercise: Name and age
-  stdout.writeln("What is your name?");
+  stdout.write("What is your name? ");
   String? userName = stdin.readLineSync();
 
-  stdout.writeln("Which year were you born?");
+  stdout.write("Which year were you born? ");
   int yearBorn = int.parse(stdin.readLineSync()!);
 
   print(
       "Hi $userName, you will be ${2021 - yearBorn} years old at the end of the year 2021");
+
+  ///Programming excercise: Seconds in a day
+  stdout.write("How many days? ");
+  int days = int.parse(stdin.readLineSync()!);
+
+  print("Seconds in that many days: ${days * 24 * 60 * 60}");
+
+  ///Programming excercise: Fix the code: Product
+  int product = 1;
+
+  stdout.write("Please type in the first number: ");
+  product *= int.parse(stdin.readLineSync()!);
+
+  stdout.write("Please type in the first number: ");
+  product *= int.parse(stdin.readLineSync()!);
+
+  stdout.write("Please type in the first number: ");
+  product *= int.parse(stdin.readLineSync()!);
+
+  print("The product is $product");
 }
