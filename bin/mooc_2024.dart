363ea1bd50d6f1a46b1139b93cc693ccd5c041dc
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-23 21:33:36 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -2,28 +2,15 @@ import 'dart:ffi';
 import 'dart:io';
 
 void main() {
-  int number1, number2;
+  stdout.write("Please type in a temperature (F): ");
+  int fahrenheit = int.parse(stdin.readLineSync()!);
 
-  stdout.write("Number 1: ");
-  number1 = int.parse(stdin.readLineSync()!);
+  double celcius = 5 / 9 * (fahrenheit - 32);
 
-  stdout.write("Number 2: ");
-  number2 = int.parse(stdin.readLineSync()!);
-
-  stdout.write("Operation: ");
-  String operation = stdin.readLineSync()!.toLowerCase();
-
-  var add = "+";
-  var multiply = "*";
-  var substract = "-";
-
-  if (operation == "add") {
-    print("$number1 $add $number2 = ${number1 + number2}");
-  } else if (operation == "multiply") {
-    print("$number1 $multiply $number2 = ${number1 * number2}");
-  } else if (operation == "substract") {
-    print("$number1 $substract $number2 = ${number1 - number2}");
+  if (celcius > 0) {
+    print("$fahrenheit degress Fahreinheit equals $celcius degress celcius");
   } else {
-    // Do nothing if operation is not recognized
+    print("$fahrenheit degress Fahreinheit equals $celcius degress celcius");
+    print("Brr! It's cold in here!");
   }
 }
--- bin/part_1/conditional_statements.dart
+++ bin/part_1/conditional_statements.dart
@@ -75,4 +75,17 @@ void main() {
   } else {
     // Do nothing if operation is not recognized
   }
+
+  ///Programming excercise: Temperatures
+  stdout.write("Please type in a temperature (F): ");
+  int fahrenheit = int.parse(stdin.readLineSync()!);
+
+  double celcius = 5 / 9 * (fahrenheit - 32);
+
+  if (celcius > 0) {
+    print("$fahrenheit degress Fahreinheit equals $celcius degress celcius");
+  } else {
+    print("$fahrenheit degress Fahreinheit equals $celcius degress celcius");
+    print("Brr! It's cold in here!");
+  }
 }
