57a4f2a78bcdf64669b770af5a9926b58db0edc7
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-23 21:20:35 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,5 +1,29 @@
-import 'package:mooc_2024/mooc_2024.dart' as mooc_2024;
+import 'dart:ffi';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${mooc_2024.calculate()}!');
+void main() {
+  int number1, number2;
+
+  stdout.write("Number 1: ");
+  number1 = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Number 2: ");
+  number2 = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Operation: ");
+  String operation = stdin.readLineSync()!.toLowerCase();
+
+  var add = "+";
+  var multiply = "*";
+  var substract = "-";
+
+  if (operation == "add") {
+    print("$number1 $add $number2 = ${number1 + number2}");
+  } else if (operation == "multiply") {
+    print("$number1 $multiply $number2 = ${number1 * number2}");
+  } else if (operation == "substract") {
+    print("$number1 $substract $number2 = ${number1 - number2}");
+  } else {
+    // Do nothing if operation is not recognized
+  }
 }
