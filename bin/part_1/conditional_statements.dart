5525785ae965191388cfa081067395050dedd1a5
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-23 21:19:36 +0700

    commit

--- bin/part_1/conditional_statements.dart
+++ bin/part_1/conditional_statements.dart
@@ -56,9 +56,23 @@ void main() {
   stdout.write("Number 1: ");
   number1 = int.parse(stdin.readLineSync()!);
 
-  stdout.write("Number 1: ");
+  stdout.write("Number 2: ");
   number2 = int.parse(stdin.readLineSync()!);
 
   stdout.write("Operation: ");
-  String? operator = stdin.readLineSync();
+  String operation = stdin.readLineSync()!.toLowerCase();
+
+  var add = "+";
+  var multiply = "*";
+  var substract = "-";
+
+  if (operation == "add") {
+    print("$number1 $add $number2 = ${number1 + number2}");
+  } else if (operation == "multiply") {
+    print("$number1 $multiply $number2 = ${number1 * number2}");
+  } else if (operation == "substract") {
+    print("$number1 $substract $number2 = ${number1 - number2}");
+  } else {
+    // Do nothing if operation is not recognized
+  }
 }
