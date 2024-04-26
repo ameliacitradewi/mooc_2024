3233b6b0ec9fc0f39690d923d899488c90bd324d
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-26 14:15:26 +0700

    commit

--- bin/part_2/programming_terminology.dart
+++ bin/part_2/programming_terminology.dart
@@ -1,3 +1,16 @@
+import 'dart:io';
+
 void main() {
   ///Programming excercise: Fix the syntax
+  stdout.write("Please type in a number: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number > 100) {
+    print("The number was greather than one hundred");
+    number = number - 100;
+    print("Now its value has decreased by one hundred");
+    print("Its value is now $number");
+  }
+
+  print("$number must be my lucky number!\nHave a nice day!");
 }
