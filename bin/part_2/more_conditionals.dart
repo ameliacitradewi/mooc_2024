a62d1c4a565406e2ba8df86ddd3d65ba7dad3a26
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-26 14:30:51 +0700

    commit

--- bin/part_2/more_conditionals.dart
+++ bin/part_2/more_conditionals.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'dart:io';
+
+void main() {
+  ///Programming excercise: Age of maturity
+  stdout.write("How old are you? ");
+  int age = int.parse(stdin.readLineSync()!);
+
+  if (age >= 18) {
+    print("You are of age!");
+  } else {
+    print("You are not of age!");
+  }
+}
