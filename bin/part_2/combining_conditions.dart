54d013656b68aaa3de4b2a7935e990564f961b15
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-28 19:06:04 +0700

    commit

--- bin/part_2/combining_conditions.dart
+++ bin/part_2/combining_conditions.dart
@@ -1,1 +1,15 @@
+import 'dart:io';
 
+void main() {
+  ///Programming excercise: Age check
+  stdout.write("What is your age? ");
+  int age = int.parse(stdin.readLineSync()!);
+
+  if (age > 5) {
+    print("Ok, you're $age years old");
+  } else if (age <= 5 && age > 0) {
+    print("I suspect you can't write quite yet...");
+  } else {
+    print("That must be a mistake");
+  }
+}
