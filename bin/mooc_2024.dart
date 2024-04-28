b5362bf7681da7dbb9adc91bdde91aca610270fe
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-28 18:42:58 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,17 +1,3 @@
 import 'dart:io';
 
-void main() {
-  stdout.write("Please type in the 1st word: ");
-  String firstWord = stdin.readLineSync()!.toLowerCase();
-
-  stdout.write("Please type in the 2nd word: ");
-  String secondWord = stdin.readLineSync()!.toLowerCase();
-
-  if (firstWord == secondWord) {
-    print("You gave the same word twice.");
-  } else {
-    var words = [firstWord, secondWord];
-    words.sort((a, b) => a.compareTo(b));
-    print("${words[1]} comes alphabetically last.");
-  }
-}
+void main() {}
