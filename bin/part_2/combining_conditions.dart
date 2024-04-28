b9595e83fa451344da8acc03d9cbeaf338f5baca
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-28 20:08:54 +0700

    commit

--- bin/part_2/combining_conditions.dart
+++ bin/part_2/combining_conditions.dart
@@ -26,4 +26,25 @@ void main() {
   }
 
   ///Programming excercise: Grades and points
+  stdout.write("How many points [0-100]: ");
+  int points = int.parse(stdin.readLineSync()!);
+
+  String grade;
+  if (points >= 0 && points <= 49) {
+    grade = "fail";
+  } else if (points >= 50 && points <= 59) {
+    grade = "1";
+  } else if (points >= 60 && points <= 69) {
+    grade = "2";
+  } else if (points >= 70 && points <= 79) {
+    grade = "3";
+  } else if (points >= 80 && points <= 89) {
+    grade = "4";
+  } else if (points >= 90 && points <= 100) {
+    grade = "5";
+  } else {
+    grade = "impossible!";
+  }
+
+  print("Grade: $grade");
 }
