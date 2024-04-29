544c8c46fdca4e8a2b7c1497abf534dd53bc9b70
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-29 13:57:01 +0700

    commit

--- bin/part_2/combining_conditions.dart
+++ bin/part_2/combining_conditions.dart
@@ -92,4 +92,26 @@ void main() {
   print("The letter in the middle is ${letters[1]}");
 
   ///Programming excercise: Gift tax calculator
+  stdout.write("Value of gift: ");
+  int gift = int.parse(stdin.readLineSync()!);
+
+  double tax;
+
+  if (gift >= 5000) {
+    if (gift < 25000) {
+      tax = 100 + ((gift - 5000) * 0.08);
+    } else if (gift < 55000) {
+      tax = 1700 + ((gift - 25000) * 0.1);
+    } else if (gift < 200000) {
+      tax = 4700 + ((gift - 55000) * 0.12);
+    } else if (gift < 1000000) {
+      tax = 22100 + ((gift - 200000) * 0.15);
+    } else {
+      tax = 142100 + ((gift - 1000000) * 0.17);
+    }
+    print("Amount of tax: ${tax.toStringAsFixed(1)} euros");
+    //nested conditionals:
+  } else {
+    print("No tax!");
+  }
 }
