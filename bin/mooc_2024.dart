d9e75d96ffd0d7b1f2fe1c4a8a71fc6d8c68b1ca
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-29 16:10:34 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,3 +1,20 @@
 import 'dart:io';
+import 'dart:math';
 
-void main() {}
+void main() {
+  int number = 0;
+
+  while (true) {
+    stdout.write("Please type in a number: ");
+    number = int.parse(stdin.readLineSync()!);
+
+    if (number > 0) {
+      print(sqrt(number).toStringAsFixed(1));
+    } else if (number < 0) {
+      print("Invalid number");
+    } else {
+      print("Exiting...");
+      break;
+    }
+  }
+}
--- bin/part_2/simple_loops.dart
+++ bin/part_2/simple_loops.dart
@@ -11,4 +11,6 @@ void main() {
     }
   }
   print("okay then");
+
+  ///Programming excercise: Input validation
 }
