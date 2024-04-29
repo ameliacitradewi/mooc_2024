4a7e1f5c59857f442a3f6617327149a4e276fab1
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-29 15:50:00 +0700

    commit

--- bin/part_2/simple_loops.dart
+++ bin/part_2/simple_loops.dart
@@ -1,4 +1,14 @@
+import 'dart:io';
+
 void main() {
   ///Programming excercise: Shall we continue?
-  while (true) {}
+  while (true) {
+    stdout.write("hi\nShall we continue? ");
+    String answer = stdin.readLineSync()!.toLowerCase();
+
+    if (answer == "no") {
+      break;
+    }
+  }
+  print("okay then");
 }
