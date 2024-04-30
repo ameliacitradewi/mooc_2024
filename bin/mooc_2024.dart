f85899898cce9ee9485c46908a8b2a87b9800c48
ameliacitradewi <ameliacitradewi@gmail.com>
2024-04-30 13:38:54 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,23 +1,3 @@
 import 'dart:io';
 
-void main() {
-  int correctPIN = 4321;
-  int attempts = 0;
-
-  while (true) {
-    stdout.write("PIN: ");
-    int enteredPIN = int.parse(stdin.readLineSync()!);
-    attempts++;
-
-    if (enteredPIN != correctPIN) {
-      print("Wrong");
-    } else {
-      if (attempts == 1) {
-        print("Correct! It only took you one single attempt!");
-      } else {
-        print("Correct! It took you $attempts attempts");
-      }
-      break;
-    }
-  }
-}
+void main() {}
