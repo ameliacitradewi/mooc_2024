a664182df4a5fe06f077299e01a367433ce230cd
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-01 11:41:45 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -4,13 +4,25 @@ void main() {
   stdout.write("Year: ");
   int year = int.parse(stdin.readLineSync()!);
 
-  int nextYear = year + 1;
+  int nextLeapYear = findNextLeapYear(year);
 
+  print("The next leap year after $year is $nextLeapYear");
+}
+
+bool isLeapYear(int year) {
+  if (year % 4 == 0) {
+    if (year % 100 != 0 || year % 400 == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int findNextLeapYear(int year) {
   while (true) {
-    if ((nextYear % 4 == 0 && nextYear % 100 != 0) || nextYear % 400 == 0) {
-      print("The next leap year after $year is $nextYear");
-      nextYear++;
-      break;
+    year++;
+    if (isLeapYear(year)) {
+      return year;
     }
   }
 }
