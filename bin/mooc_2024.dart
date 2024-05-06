c7b563ad4dc6cd7b4611ccb3a4cf39572e5ee46c
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-06 15:59:08 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,31 +1,13 @@
 import 'dart:io';
 
 void main() {
-  // Part 1
-  // String story = "";
-
-  // while (true) {
-  //   stdout.write("Please type in a word: ");
-  //   String word = stdin.readLineSync()!;
-
-  //   if (word != "end") {
-  //     story += word + " ";
-  //   } else {
-  //     break;
-  //   }
-  // }
-
-  // print(story);
-
-  // Part 2
   List<String> words = [];
-  String? input;
 
   while (true) {
     stdout.write("Please type in a word: ");
-    input = stdin.readLineSync()!;
+    String input = stdin.readLineSync()!;
 
-    if (input == "end" || input == words.last) {
+    if (input == "end" || (words.isNotEmpty && input == words.last)) {
       break;
     }
 
--- bin/part_2/simple_loops.dart
+++ bin/part_2/simple_loops.dart
@@ -117,4 +117,20 @@ void main() {
   }
 
   print(story);
+
+  //Part 2
+  List<String> words = [];
+
+  while (true) {
+    stdout.write("Please type in a word: ");
+    String input = stdin.readLineSync()!;
+
+    if (input == "end" || (words.isNotEmpty && input == words.last)) {
+      break;
+    }
+
+    words.add(input);
+  }
+
+  print(words.join(" "));
 }
