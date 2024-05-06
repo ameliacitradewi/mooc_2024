367733e8edf4b00b87a5c91a64745b3927f52544
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-06 15:40:45 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,3 +1,34 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  // Part 1
+  // String story = "";
+
+  // while (true) {
+  //   stdout.write("Please type in a word: ");
+  //   String word = stdin.readLineSync()!;
+
+  //   if (word != "end") {
+  //     story += word + " ";
+  //   } else {
+  //     break;
+  //   }
+  // }
+
+  // print(story);
+
+  // Part 2
+  List<String> sentences = [];
+
+  while (true) {
+    stdout.write("Please type in a word: ");
+    String words = stdin.readLineSync()!;
+
+    if (words != "end") {
+      sentences.add(words);
+    } else {
+      print(sentences.join(" "));
+      break;
+    }
+  }
+}
