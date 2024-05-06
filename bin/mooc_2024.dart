49e7a2dca78e8b59437b19e902f1175cf685fa8e
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-06 16:06:06 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -3,12 +3,19 @@ import 'dart:io';
 void main() {
   print("Please type in integer numbers. Type in 0 to finish.");
 
+  int hitNumber = 0;
+
   while (true) {
     stdout.write("Number: ");
     int number = int.parse(stdin.readLineSync()!);
 
-    if (number == 0) {
+    if (number != 0) {
+      hitNumber += 1;
+    } else {
       break;
     }
   }
+
+  print("... the program asks for numbers");
+  print("Numbers type in $hitNumber");
 }
--- bin/part_2/simple_loops.dart
+++ bin/part_2/simple_loops.dart
@@ -146,4 +146,25 @@ void main() {
       break;
     }
   }
+
+  //Part 1: Count
+  print("Please type in integer numbers. Type in 0 to finish.");
+
+  int hitNumber = 0;
+
+  while (true) {
+    stdout.write("Number: ");
+    int number = int.parse(stdin.readLineSync()!);
+
+    if (number != 0) {
+      hitNumber += 1;
+    } else {
+      break;
+    }
+  }
+
+  print("... the program asks for numbers");
+  print("Numbers type in $hitNumber");
+
+  //Part 2: Sum
 }
