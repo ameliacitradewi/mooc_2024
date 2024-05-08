5b6dadc6e1be08786e01f5014009e17c7653c7c0
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-08 14:35:29 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,3 +1,12 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  int number = 1;
+
+  while (number < 31) {
+    if (number % 2 == 0) {
+      print(number);
+    }
+    number++;
+  }
+}
---
+++ bin/part_3/loops_with_conditions.dart
@@ -0,0 +1,11 @@
+void main() {
+  //Programming exercise: Print numbers
+  int number = 1;
+
+  while (number < 31) {
+    if (number % 2 == 0) {
+      print(number);
+    }
+    number++;
+  }
+}
