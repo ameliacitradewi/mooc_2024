f210b7eee7e9804e64a39e7e03136ec65b511619
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-09 10:11:49 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -2,22 +2,21 @@ import 'dart:io';
 
 void main() {
   stdout.write("Limit: ");
-  int userLimit = int.parse(stdin.readLineSync()!);
+  int inputLimit = int.parse(stdin.readLineSync()!);
 
-  int sumAll = 0;
-  int nextNumber = 1;
+  int sumNumbers = 0;
+  int nextNumbers = 1;
   String calculation = "";
 
-  while (sumAll < userLimit) {
-    sumAll += nextNumber;
-    calculation += nextNumber.toString();
+  while (sumNumbers < inputLimit) {
+    sumNumbers += nextNumbers;
+    calculation += nextNumbers.toString();
+    nextNumbers++;
 
-    if (nextNumber <= userLimit) {
+    if (sumNumbers < inputLimit) {
       calculation += " + ";
     }
-
-    nextNumber++;
   }
 
-  print("The consecutive sum: $calculation = $sumAll");
+  print("The consecutive sum: $calculation = $sumNumbers");
 }
--- bin/part_3/loops_with_conditions.dart
+++ bin/part_3/loops_with_conditions.dart
@@ -74,4 +74,22 @@ void main() {
   print(sumAll);
 
   //Programming exercise: The sum of consecutive numbers, version 2
+  stdout.write("Limit: ");
+  int inputLimit = int.parse(stdin.readLineSync()!);
+
+  int sumNumbers = 0;
+  int nextNumbers = 1;
+  String calculation = "";
+
+  while (sumNumbers < inputLimit) {
+    sumNumbers += nextNumbers;
+    calculation += nextNumbers.toString();
+    nextNumbers++;
+
+    if (sumNumbers < inputLimit) {
+      calculation += " + ";
+    }
+  }
+
+  print("The consecutive sum: $calculation = $sumNumbers");
 }
