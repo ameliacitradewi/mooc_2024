9e26ce89fbba921eec8a9d85a2eabcebaa701c9e
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-09 13:16:02 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,22 +1,3 @@
 import 'dart:io';
 
-void main() {
-  stdout.write("Width: ");
-  int inputWidth = int.parse(stdin.readLineSync()!); //n
-
-  stdout.write("Height: ");
-  int inputHeight = int.parse(stdin.readLineSync()!); //row
-
-  int height = 0;
-
-  while (height < inputHeight) {
-    String hashes = "";
-    int width = 0;
-    while (width < inputWidth) {
-      hashes += "#";
-      width++;
-    }
-    print(hashes);
-    height++;
-  }
-}
+void main() {}
