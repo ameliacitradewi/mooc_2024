d84cd4f9c7203f998999633783a980cda610865b
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-09 12:23:51 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,3 +1,13 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  stdout.write("Please type in a string: ");
+  String inputWord = stdin.readLineSync()!;
+
+  int wordLength = inputWord.length;
+
+  while (wordLength > 0) {
+    print(inputWord[wordLength - 1]);
+    wordLength--;
+  }
+}
--- bin/part_3/working_with_strings.dart
+++ bin/part_3/working_with_strings.dart
@@ -26,4 +26,13 @@ void main() {
   }
 
   ///Programming exercise: End to beginning
+  stdout.write("Please type in a string: ");
+  String inputWord = stdin.readLineSync()!;
+
+  int wordLength = inputWord.length;
+
+  while (wordLength > 0) {
+    print(inputWord[wordLength - 1]);
+    wordLength--;
+  }
 }
