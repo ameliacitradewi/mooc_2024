5b4e0d5c029e4161d739788fbefdfbcf48663e8e
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-09 12:08:50 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,11 +1,17 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Please type in a string: ");
-  String text = stdin.readLineSync()!;
+  stdout.write("Please type in string 1: ");
+  String string1 = stdin.readLineSync()!;
 
-  stdout.write("Please type in an amount: ");
-  int multiplied = int.parse(stdin.readLineSync()!);
+  stdout.write("Please type in string 2: ");
+  String string2 = stdin.readLineSync()!;
 
-  print(text * multiplied);
+  if (string1.length > string2.length) {
+    print("$string1 is longer");
+  } else if (string1.length < string2.length) {
+    print("$string2 is longer");
+  } else {
+    print("The strings are equally long");
+  }
 }
