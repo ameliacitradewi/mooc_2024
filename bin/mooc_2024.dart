46dc96f063f4e4bf863e5cdb756ad1e5bfe800ee
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-13 13:33:39 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,17 +1,3 @@
 import 'dart:io';
 
-void main() {
-  stdout.write("Word: ");
-  String frameWord = stdin.readLineSync()!;
-
-  int widthFrame = 30;
-  int length = frameWord.length;
-  String frame = "*" * 30;
-
-  int leftSpace = ((widthFrame - length - 2) / 2).ceil().toInt();
-  int rightSpace = ((widthFrame - length - 2) / 2).floor().toInt();
-
-  print(frame);
-  print("*${" " * leftSpace}$frameWord${" " * rightSpace}*");
-  print(frame);
-}
+void main() {}
