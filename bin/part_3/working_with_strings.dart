8cb915421b228fa195ec16aaeecd331f42cdbf14
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-13 14:04:12 +0700

    commit

--- bin/part_3/working_with_strings.dart
+++ bin/part_3/working_with_strings.dart
@@ -110,4 +110,13 @@ void main() {
   print(frame);
 
   ///Programming exercise: Substrings, part 1
+  stdout.write("Please type in a string: ");
+  String wordString = stdin.readLineSync()!;
+  int indexString = wordString.length;
+  int nextIndex = 0;
+
+  while (nextIndex <= indexString) {
+    print(wordString.substring(0, nextIndex));
+    nextIndex++;
+  }
 }
