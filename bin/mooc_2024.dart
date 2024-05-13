7bd301abfa552e6b416a85a56e3c946abeaa3d07
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-13 13:33:18 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -5,5 +5,13 @@ void main() {
   String frameWord = stdin.readLineSync()!;
 
   int widthFrame = 30;
-  int fwLength = frameWord.length;
+  int length = frameWord.length;
+  String frame = "*" * 30;
+
+  int leftSpace = ((widthFrame - length - 2) / 2).ceil().toInt();
+  int rightSpace = ((widthFrame - length - 2) / 2).floor().toInt();
+
+  print(frame);
+  print("*${" " * leftSpace}$frameWord${" " * rightSpace}*");
+  print(frame);
 }
--- bin/part_3/working_with_strings.dart
+++ bin/part_3/working_with_strings.dart
@@ -95,4 +95,17 @@ void main() {
   }
 
   ///Programming exercise: A frame word
+  stdout.write("Word: ");
+  String frameWord = stdin.readLineSync()!;
+
+  int widthFrame = 30;
+  int length = frameWord.length;
+  String frame = "*" * 30;
+
+  int leftSpace = ((widthFrame - length - 2) / 2).ceil().toInt();
+  int rightSpace = ((widthFrame - length - 2) / 2).floor().toInt();
+
+  print(frame);
+  print("*${" " * leftSpace}$frameWord${" " * rightSpace}*");
+  print(frame);
 }
