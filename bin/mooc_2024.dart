dcc41a8d349b35f7d0420c9551795ade137ecc37
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-14 15:22:29 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -2,19 +2,4 @@ import 'dart:io';
 
 import 'part_1/arithmetic_operations.dart';
 
-void main() {
-  stdout.write("Please type in a word: ");
-  String inputValue = stdin.readLineSync()!;
-
-  stdout.write("Please type in a character: ");
-  String char = stdin.readLineSync()!;
-
-  int indexChar = inputValue.indexOf(char);
-
-  if (indexChar != -1 && indexChar <= inputValue.length - 3) {
-    String result = inputValue.substring(indexChar, indexChar + 3);
-    if (result.length >= 3) {
-      print(result);
-    }
-  }
-}
+void main() {}
