f2c29b5d0fdd90c82030f4f1a1d900055e3d4578
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-14 13:27:11 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,12 +1,3 @@
 import 'dart:io';
 
-void main() {
-  stdout.write('Please type in a string: ');
-  String strings = stdin.readLineSync()!;
-
-  int i = 0;
-  while (i <= strings.length) {
-    print(strings.substring(strings.length - i));
-    i++;
-  }
-}
+void main() {}
