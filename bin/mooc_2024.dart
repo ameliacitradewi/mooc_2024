54101c85d18e21b3f863ea04ee53bad5a11ef4f5
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-14 13:48:03 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,3 +1,19 @@
 import 'dart:io';
+import 'dart:core';
 
-void main() {}
+void main() {
+  stdout.write("Please type in a string: ");
+  String inputString = stdin.readLineSync()!;
+
+  void checkVowels(String vowels) {
+    if (inputString.contains(vowels)) {
+      print("$vowels found");
+    } else {
+      print("$vowels not found");
+    }
+  }
+
+  checkVowels("a");
+  checkVowels("e");
+  checkVowels("o");
+}
