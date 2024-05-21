22fc2c3228e3997a797c89b25204b16ae203e277
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-21 21:37:09 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,22 +1,3 @@
 import 'dart:io';
 
-void main() {
-  stdout.write("Please type in a string: ");
-  String mainString = stdin.readLineSync()!;
-
-  stdout.write("Please type in a substring: ");
-  String subString = stdin.readLineSync()!;
-
-  int firstIndex = mainString.indexOf(subString);
-
-  if (firstIndex != -1) {
-    int secondIndex =
-        mainString.indexOf(subString, firstIndex + subString.length);
-
-    if (secondIndex == -1) {
-      print("The substring does not occur twice in the string.");
-    } else {
-      print("The second occurrence of the substring is at index $secondIndex.");
-    }
-  }
-}
+void main() {}
