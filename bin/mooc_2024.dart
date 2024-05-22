e029e242a40918f1afb36467dd8a803924f70be6
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-22 23:04:15 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -2,16 +2,5 @@ import 'dart:io';
 
 void main() {
   stdout.write("Please type in a number: ");
-  int number = int.parse(stdin.readLineSync()!);
-
-  int i = 1;
-
-  while (i <= number) {
-    int j = 1;
-    while (j <= number) {
-      print("$i * $j = ${i * j}");
-      j++;
-    }
-    i++;
-  }
+  String sentences = stdin.readLineSync()!;
 }
