5c3f30edbc2504e4de4e97db5f50d1f2baf7ae72
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-22 22:42:43 +0700

    commit

---
+++ bin/part_3/more_loops.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  ///Programming excercise: Multiplication
+  stdout.write("Please type in a number: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  int i = 1;
+
+  while (i <= number) {
+    int j = 1;
+    while (j <= number) {
+      print("$i * $j = ${i * j}");
+      j++;
+    }
+    i++;
+  }
+}
