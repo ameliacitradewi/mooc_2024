a39ab32055677e0b31632c77891813aa7b94371a
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-22 23:41:34 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,3 +1,19 @@
 import 'dart:io';
 
-void main() {}
+void main() {
+  int factorial(int n) {
+    return n == 1 ? 1 : n * factorial(n - 1);
+  }
+
+  while (true) {
+    stdout.write("Please type in a number: ");
+    int input = int.parse(stdin.readLineSync()!);
+
+    if (input <= 0) {
+      print("Thanks and bye!");
+      break;
+    } else {
+      print("The factorial of the number $input is ${factorial(input)}");
+    }
+  }
+}
--- bin/part_3/more_loops.dart
+++ bin/part_3/more_loops.dart
@@ -27,4 +27,19 @@ void main() {
   }
 
   ///Programming excercise: Factorial
+  int factorial(int n) {
+    return n == 1 ? 1 : n * factorial(n - 1);
+  }
+
+  while (true) {
+    stdout.write("Please type in a number: ");
+    int input = int.parse(stdin.readLineSync()!);
+
+    if (input <= 0) {
+      print("Thanks and bye!");
+      break;
+    } else {
+      print("The factorial of the number $input is ${factorial(input)}");
+    }
+  }
 }
