0513fd769f32531074b76e1944178f219fa0c32e
ameliacitradewi <ameliacitradewi@gmail.com>
2024-05-27 19:55:01 +0700

    commit

--- bin/mooc_2024.dart
+++ bin/mooc_2024.dart
@@ -1,22 +1,3 @@
 import 'dart:io';
 
-void main() {
-  // factorial using recursion
-  // check if the input == 1, if true return 1
-  // if false, return input * factorial (input - 1)
-  int factorial(int n) {
-    return n == 1 ? 1 : n * factorial(n - 1);
-  }
-
-  while (true) {
-    stdout.write("Please type in a number: ");
-    int input = int.parse(stdin.readLineSync()!);
-
-    if (input > 0) {
-      print("The factorial of the number $input is ${factorial(input)}");
-    } else {
-      print("Thanks and bye!");
-      break;
-    }
-  }
-}
+void main() {}
